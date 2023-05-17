9b734af994b6bdb2ef5c3f86829e7c6aea8b953f
mingyuchoo <mingyuchoo@gmail.com>
2023-05-18 03:39:01 +0900

    updated

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,18 +1,22 @@
 -- HACK: we can change this file name to `init.lua`
 return {
+  -- Theme plugin
   {
     "sainnhe/sonokai",
     init = function()
       vim.g.sonokai_style = "shusia"
     end,
   },
+  -- Stylish-haskell plugin
   {
     "nbouscal/vim-stylish-haskell",
     lazy = false,
   },
+  -- Copilot X plugin
   {
     "github/copilot.vim",
   },
+  -- Todo plugin
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
