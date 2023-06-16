5e936b009e2971ffe918ee0ce1da8682b71034d1
mingyuchoo <mingyuchoo@gmail.com>
2023-06-17 08:21:39 +0900

    update mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -5,6 +5,7 @@ return {
     ["<C-b>"] = { "<Left>", desc = "Emacs character backward" },
     ["<C-a>"] = { "<Home>", desc = "Emacs line start" },
     ["<C-e>"] = { "<End>", desc = "Emacs line end" },
+    ["<C-h>"] = { "<C-w>h", desc = "Move cursor to the left window" },
   },
   -- insert mode
   i = {
@@ -28,5 +29,6 @@ return {
     ["<C-h>"] = { "<Backspace>", desc = "Emacs delete forward" },
     ["<C-d>"] = { "<Del>", desc = "Emacs delete backward" },
     ["<C-k>"] = { "<Esc>d", desc = "Emacs delete to line end" },
+    ["<Esc>"] = { "<C-\\><C-n>", desc = "Exit insert mode" },
   },
 }
