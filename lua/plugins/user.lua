cb8a4f1fe92e75dc4cb947a579c653dd19f0a24c
mingyuchoo <mingyuchoo@gmail.com>
2024-05-10 16:05:47 +0900

    updated

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -6,119 +6,153 @@
 ---@type LazySpec
 return {
 
-  -- == Examples of Adding Plugins ==
+	-- == Examples of Adding Plugins ==
 
-  "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
+	"andweeb/presence.nvim",
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "BufRead",
+		config = function()
+			require("lsp_signature").setup()
+		end,
+	},
 
-  -- == Examples of Overriding Plugins ==
+	-- == Examples of Overriding Plugins ==
 
-  -- customize alpha options
-  {
-    "goolord/alpha-nvim",
-    opts = function(_, opts)
-      -- customize the dashboard header
-      opts.section.header.val = {
-        " █████  ███████ ████████ ██████   ██████",
-        "██   ██ ██         ██    ██   ██ ██    ██",
-        "███████ ███████    ██    ██████  ██    ██",
-        "██   ██      ██    ██    ██   ██ ██    ██",
-        "██   ██ ███████    ██    ██   ██  ██████",
-        " ",
-        "    ███    ██ ██    ██ ██ ███    ███",
-        "    ████   ██ ██    ██ ██ ████  ████",
-        "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-        "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-        "    ██   ████   ████   ██ ██      ██",
-      }
-      return opts
-    end,
-  },
+	-- customize alpha options
+	{
+		"goolord/alpha-nvim",
+		opts = function(_, opts)
+			-- customize the dashboard header
+			opts.section.header.val = {
+				" █████  ███████ ████████ ██████   ██████",
+				"██   ██ ██         ██    ██   ██ ██    ██",
+				"███████ ███████    ██    ██████  ██    ██",
+				"██   ██      ██    ██    ██   ██ ██    ██",
+				"██   ██ ███████    ██    ██   ██  ██████",
+				" ",
+				"    ███    ██ ██    ██ ██ ███    ███",
+				"    ████   ██ ██    ██ ██ ████  ████",
+				"    ██ ██  ██ ██    ██ ██ ██ ████ ██",
+				"    ██  ██ ██  ██  ██  ██ ██  ██  ██",
+				"    ██   ████   ████   ██ ██      ██",
+			}
+			return opts
+		end,
+	},
 
-  -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = false },
+	-- You can disable default plugins as follows:
+	{ "max397574/better-escape.nvim", enabled = false },
 
-  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
-    end,
-  },
+	-- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
+	{
+		"L3MON4D3/LuaSnip",
+		config = function(plugin, opts)
+			require("astronvim.plugins.configs.luasnip")(plugin, opts) -- include the default astronvim config that calls the setup call
+			-- add more custom luasnip configuration such as filetype extend or custom snippets
+			local luasnip = require("luasnip")
+			luasnip.filetype_extend("javascript", { "javascriptreact" })
+		end,
+	},
 
-  {
-    "windwp/nvim-autopairs",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom autopairs configuration such as custom rules
-      local npairs = require "nvim-autopairs"
-      local Rule = require "nvim-autopairs.rule"
-      local cond = require "nvim-autopairs.conds"
-      npairs.add_rules(
-        {
-          Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
-        },
-        -- disable for .vim files, but it work for another filetypes
-        Rule("a", "a", "-vim")
-      )
-      -- for Elixir
-      npairs.add_rules {
-        Rule(" do", " end", "elixir"),
-      }
-    end,
-  },
-  -- ####################################################
-  -- Added by @mingyuchoo
-  -- ####################################################
-  {
-    "cormacrelf/vim-colors-github",
-    init = function() vim.g.background = "light" end,
-  },
-  -- Stylish-haskell plugin
-  {
-    "nbouscal/vim-stylish-haskell",
-    lazy = false,
-  },
-  -- Copilot X plugin
-  {
-    "github/copilot.vim",
-  },
-  -- Todo plugin
-  {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {},
-    event = "User AstroFile",
-  },
-  -- Vim-dadbod
-  {
-    "kristijanhusak/vim-dadbod-ui",
-    lazy = false,
-  },
-  {
-    "tpope/vim-dadbod",
-    lazy = false,
-  },
-  {
-    "hkupty/iron.nvim",
-  },
+	{
+		"windwp/nvim-autopairs",
+		config = function(plugin, opts)
+			require("astronvim.plugins.configs.nvim-autopairs")(plugin, opts) -- include the default astronvim config that calls the setup call
+			-- add more custom autopairs configuration such as custom rules
+			local npairs = require("nvim-autopairs")
+			local Rule = require("nvim-autopairs.rule")
+			local cond = require("nvim-autopairs.conds")
+			npairs.add_rules(
+				{
+					Rule("$", "$", { "tex", "latex" })
+						-- don't add a pair if the next character is %
+						:with_pair(cond.not_after_regex("%%"))
+						-- don't add a pair if  the previous character is xxx
+						:with_pair(
+							cond.not_before_regex("xxx", 3)
+						)
+						-- don't move right when repeat character
+						:with_move(cond.none())
+						-- don't delete if the next character is xx
+						:with_del(cond.not_after_regex("xx"))
+						-- disable adding a newline when you press <cr>
+						:with_cr(cond.none()),
+				},
+				-- disable for .vim files, but it work for another filetypes
+				Rule("a", "a", "-vim")
+			)
+			-- for Elixir
+			npairs.add_rules({
+				Rule(" do", " end", "elixir"),
+			})
+		end,
+	},
+	-- ####################################################
+	-- Added by @mingyuchoo
+	-- ####################################################
+	{
+		"cormacrelf/vim-colors-github",
+		init = function()
+			vim.g.background = "light"
+		end,
+	},
+	-- Stylish-haskell plugin
+	{
+		"nbouscal/vim-stylish-haskell",
+		lazy = false,
+	},
+	-- Copilot X plugin
+	{
+		"github/copilot.vim",
+	},
+	-- Todo plugin
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
+		event = "User AstroFile",
+	},
+	-- Vim-dadbod
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		lazy = false,
+	},
+	{
+		"tpope/vim-dadbod",
+		lazy = false,
+	},
+	{
+		"hkupty/iron.nvim",
+	},
+	-- Elixir-tools
+	{
+		"elixir-tools/elixir-tools.nvim",
+		version = "*",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local elixir = require("elixir")
+			local elixirls = require("elixir.elixirls")
+
+			elixir.setup({
+				nextls = { enable = true },
+				credo = {},
+				elixirls = {
+					enable = true,
+					settings = elixirls.settings({
+						dialyzerEnabled = false,
+						enableTestLenses = false,
+					}),
+					on_attach = function(client, bufnr)
+						vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
+						vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
+						vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+					end,
+				},
+			})
+		end,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
 }
