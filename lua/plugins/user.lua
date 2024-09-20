124da56e67cb49d2db85729a5d7aa8ceb7c7f2a0
mingyuchoo <mingyuchoo@gmail.com>
2024-09-20 17:08:08 +0900

    Update user.lua

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -102,12 +102,6 @@ return {
 		"nbouscal/vim-stylish-haskell",
 		lazy = false,
 	},
-	-- Rust plugin
-	{
-		'mrcjkb/rustaceanvim',
-		version = '^5', -- Recommended
-		lazy = false, -- This plugin is already lazy
-        },
 	-- Copilot X plugin
 	{
 		"github/copilot.vim",
@@ -160,5 +154,60 @@ return {
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 		},
+	},
+	-- Rust plugin
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^5", -- Recommended
+		lazy = false, -- This plugin is already lazy
+		config = function()
+			local mason_registry = require "mason-registry"
+			local codelldb = mason_registry.get_package "codelldb"
+			local extension_path = codelldb:get_install_path() .. "/extension/"
+			local codelldb_path = extension_path .. "adapter/codelldb"
+			local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+			local cfg = require "rustaceanvim.config"
+			vim.g.rustaceanvim = {
+				dap = {
+					adaper = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+				},
+			}
+		end,
+	},
+ 	{
+		"mfussenegger/nvim-dap",
+		config = function()
+			local dap, dapui = require "dap", require "dapui"
+			dap.listeners.before.attach.dapui_config = function() dapui.open() end
+			dap.listeners.before.launch.dapui_config = function() dapui.open() end
+			dap.listeners.before.event_terminated.dapui_config = function() dapui.open() end
+			dap.listeners.before.event_exited.dapui_config = function() dapui.open() end
+		end,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+		config = function() require("dapui").setup() end,
+	},
+	{
+		"rust-lang/rust.vim",
+		ft = "rust",
+		init = function() vim.g.rustfmt_autosave = 1 end,
+	},
+ 	{
+		"saecki/crates.nvim",
+		ft = { "toml" },
+		config = function()
+			require("crates").setup {
+				completion = {
+					cmp = {
+						enabled = true,
+					},
+				},
+			}
+			require("cmp").setup.buffer {
+				sources = { { name = "crates" } },
+			}
+		end,
 	},
 }
